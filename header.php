61571c3998e7f58a76b8823e25d353eca47bd0c7
Marcelo <m.azevedo23@gmail.com>
2013-05-09 21:06:18 -0300

    página quem somos (html e css)

---
+++ header.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>Affinity Contábil</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width">
+
+        <!-- Lugar favicon.ico e apple-touch-icon.png no diretório raiz -->
+
+        <link rel="stylesheet" href="css/normalize.css">
+        <link rel="stylesheet" href="css/style.css">
+        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+    </head>
+    <body>
+        <!--[if lt IE 7]>
+            <p class="chromeframe">Você está usando um browser<strong>Antigo</strong>. Por Favor <a href="http://browsehappy.com/">Atualise seu browser</a> ou <a href="http://www.google.com/chromeframe/?redirect=true">ative o Google Chrome Frame</a> para melhorar a sua experiência!!</p>
+        <![endif]-->
+
+        <div id="main">
+            <header id="topo">
+                <div class="center">
+                    <a href=""><div id="logo"></div></a>
+                    <nav id="topo1">
+                        <ul>
+                            <li class="none">Idioma</li>
+                            <li class="idioma">Português</li>
+                            <li>English</li>
+                            <li>Español</li>
+                        </ul>
+                    </nav> <!-- nav Topo1 -->
+                    <nav id="topo2">
+                        <ul>
+                            <a href="">
+                                <li>Home</li>
+                            </a>
+                            <a href="">
+                                <li>Quem Somos</li>
+                            </a>
+                            <a href="">
+                                <li>Equipe</li>
+                            </a>
+                            <a href="">
+                                <li>Área de Atuação</li>
+                            </a>
+                            <a href="">
+                                <li>Serviços</li>
+                            </a>
+                            <a href="">
+                                <li>Links Úteis</li>
+                            </a>
+                            <a href="">
+                                <li>Contato</li>
+                            </a>
+                        </ul>
+                    </nav> <!-- nav Topo2 -->
+                </div> <!-- class Center -->
+            </header> <!-- header Topo -->
+            <nav id="menu">
+                <div class="center">
+                    <ul>
+                        <a href="">
+                            <li class="menu01">Modelos de </br>Documentos</li>
+                        </a>
+                        <a href="">
+                            <li class="menu02">Agenda de </br>Obrigações</li>
+                        </a>
+                        <a href="">
+                            <li class="menu03">Certidões </br>Negativas</li>
+                        </a>
+                        <a href="">
+                            <li class="menu04">Índices </br>Econômicos</li>
+                        </a>
+                        <a href="">
+                            <li class="menu05">Prática </br>Contábil</li>
+                        </a>
+                        <a href="">
+                            <li class="menu06">Tabelas </br>Praticadas</li>
+                        </a>
+                    </ul>
+                </div> <!-- class Center -->
+            </nav> <!-- nav Menu -->
